9226d0f10c3ab3a2586f04b2b6ccf4d90ff9ad39
Fakhri Wahid Athallah <fakhri.athallah@binus.edu>
2024-12-09 09:51:26 +0700

    fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["public/assets/css/app.css", "public/assets/js/app.js"],
             refresh: true,
         }),
     ],
