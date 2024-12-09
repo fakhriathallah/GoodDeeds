64c4dcc9c96b7ed8a516a41e88bbd2cb76211be4
Fakhri Wahid Athallah <fakhri.athallah@binus.edu>
2024-12-09 09:15:14 +0700

    testing deployment

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::middleware(['auth', 'verified'])->get('/dashboard', [DashboardController::class, 'generalUserDashboard'])->name('dashboard');
+
 Route::get('/', [MsDeedsController::class,'guestDashboard'])->name('home.index');
 
 
